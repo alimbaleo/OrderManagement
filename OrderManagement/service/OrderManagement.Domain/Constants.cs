461cff7ebbcbe38f0e718df2c90869f38b61aeb4
Alimba Chukwuemeka <alimbaleo@gmail.com>
2022-06-20 03:16:32 +0100

    Seed default admin user

--- OrderManagement/service/OrderManagement.Domain/Constants.cs
+++ OrderManagement/service/OrderManagement.Domain/Constants.cs
@@ -4,5 +4,7 @@
     {
         public const string ADMIN_ROLE = "ADMIN";
         public const string USER_ROLE = "USER";
+        public const string DEFAULT_ADMIN_EMAIL = "alimbaleoo@hotmail.com";
+        public const string DEFAULT_ADMIN_PASSWORD = "P@55w0rd10$";
     }
 }
--- OrderManagement/service/OrderManagement.EntityFramework/OrderManagementDBContext.cs
+++ OrderManagement/service/OrderManagement.EntityFramework/OrderManagementDBContext.cs
@@ -19,9 +19,11 @@ namespace OrderManagement.EntityFramework
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
             builder.Entity<AppUser>(b =>
             {
                 b.ToTable("AppUsers");
+                b.HasData(GetDefaultAdminUser());
             });
 
             builder.Entity<IdentityRole>(i =>
@@ -30,7 +32,31 @@ namespace OrderManagement.EntityFramework
                 i.HasData(new IdentityRole { Id = USER_ROLE, Name = USER_ROLE, NormalizedName = USER_ROLE, ConcurrencyStamp = Guid.NewGuid().ToString() }, new IdentityRole { Id = ADMIN_ROLE, Name = ADMIN_ROLE, NormalizedName = ADMIN_ROLE, ConcurrencyStamp = Guid.NewGuid().ToString() });
             });
 
+            //set user role to admin
+            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = ADMIN_ROLE,
+                UserId = DEFAULT_ADMIN_EMAIL
+            });
         }
+        private AppUser GetDefaultAdminUser()
+        {
+            var appUser = new AppUser
+            {
+                Id = DEFAULT_ADMIN_EMAIL,
+                Email = DEFAULT_ADMIN_EMAIL,
+                EmailConfirmed = true,
+                FirstName = "Admin",
+                Surname = "Admin",
+                UserName = DEFAULT_ADMIN_EMAIL,
+            NormalizedUserName = DEFAULT_ADMIN_EMAIL.ToUpper()
+            };
 
+            //set user password
+            PasswordHasher<AppUser> ph = new PasswordHasher<AppUser>();
+            appUser.PasswordHash = ph.HashPassword(appUser, DEFAULT_ADMIN_PASSWORD);
+
+            return appUser;
+        }
     }
 }
